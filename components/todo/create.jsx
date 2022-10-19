a2f40393bc749c2ca92387883f7138f4423d3d7f
Mikhael Bailly <Mikhael.bailly1@orange.fr>
2022-10-19 17:08:33 +0200

    fix the task creation and tasks order

--- components/todo/create.jsx
+++ components/todo/create.jsx
@@ -1,20 +1,19 @@
-import React, { useRef} from "react";
+import React, { useRef, useEffect, useMemo } from "react";
 import { useForm } from "react-hook-form";
 import { Plus } from 'react-feather';
 
 const TodoCreate = ({createHandler}) => {
-    const { register, handleSubmit } = useForm();
-    const ref = useRef(null);
-
-    const resetInput = () => {
-        ref.current.value = '';
-    };
+    const { register, reset, handleSubmit } = useForm();
 
     return (
-        <form onSubmit={handleSubmit(() => createHandler() || resetInput())}>
-            <div className="flex items-center mt-4 mb-10">
-                <label htmlFor="name" className="text-blue-500"><Plus /></label>
-                <input {...register("task")} ref={ref} placeholder="Add a new task and press ENTER..." type="text" id="name" className="text-sm py-3 px-2 w-full bg-transparent"/>
+        <form onSubmit={handleSubmit((data) => createHandler(data) || reset())}>
+            <div className="flex items-center mt-4 mb-10 relative">
+                <label htmlFor="task" className="text-blue-500"><Plus /></label>
+                <input {...register("task")} placeholder="Add a new task..." type="text" id="task" className="text-sm py-3 px-2 w-full bg-transparent"/>
+                
+                <button type="submit" className="absolute right-2 px-2 py-2 text-xs tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                    Create
+                </button>
             </div>
         </form>
     );
